1e05ce82e36964a07e1686904fa6a50953c9fb84
Gustavo Fernandez <gfernandez@uea.edu.ec>
2018-08-27 14:53:04 -0500

    Archivos de configuración para distribución 'dist'

--- config/params-dist.php
+++ config/params-dist.php
@@ -3,7 +3,7 @@
 return [
 
     //Atributos Personalizados de Active Directory o LDAP
-    'dni' => 'dni',
+    'dni'          => 'dni',
     'personalmail' => 'personalmail',
 
     //Para agregar los atributos personalizados en Active Directory lea los siguientes enlaces:
@@ -12,24 +12,24 @@ return [
 
 
     //Parámetros Personalizados de la Companía, Empresa o Institución
-    'company' => 'El Nombre de la Empresa',
+    'company'    => 'Nombre de la Empresa',
     'adminEmail' => 'admin@dominio.com', //Cuenta para recibir correo del formulario de contacto.
-    'appURL' => 'http://sitic.dominio.com/',
-    'contact' => 'Unidad de TI. Contáctese al (+593) 3-2555-555 en horario de atención de lunes a viernes de 08h00 - 17h00.',
+    'appURL'     => 'http://sitic.dominio.com/',
+    'contact'    => 'Unidad de TI. Contáctese al (+593) 3-2555-555 de lunes a viernes, en horario de atención 08h00 - 17h00.',
 
 
     //Parámetros Personalizados Email Sender Gestión de identidad
-    'from' => 'usuario@dominio.com',
+    'from'     => 'usuario@dominio.com',
     'fromName' => 'Sistema de Gestión de Identidad',
-    'cc' => 'usuario@dominio.com', //Opción para almacenar copias de Emails Enviados
-    'subject' => 'Restablecer Contraseña',
+    'cc'       => 'usuario@dominio.com', //Opción para almacenar copias de Emails Enviados
+    'subject'  => 'Restablecer Contraseña',
 
 
     //Generar un Código de Seguridad (SaltKey) en el siguiente enlace
     //http://www.sethcardoza.com/tools/random-password-generator/
     //Tested -> 40 Characters Long (Good for Cakephp Security Salt)
-    'saltKey' => 'SvaD6oysdY4oGh97D9Ejtw6RXROr85Olq2yxBjH7',
-    'algorithm' => 'sha256',
+    'saltKey'         => 'SvaD6oysdY4oGh97D9Ejtw6RXROr85Olq2yxBjH7',
+    'algorithm'       => 'sha256',
     'tokenDateFormat' => date('Y-m-d'), //Duración de token en fecha actual desde 00:00 hasta 23:59
 
 ];
