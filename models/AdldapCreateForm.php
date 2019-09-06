f81ff397d49f59baad59ec9b38c28c125e5e8b55
Gustavo Fernandez <gfernandez@uea.edu.ec>
2019-09-06 14:16:27 -0500

    Actualizacion formulario creacion ADLAP - Control de caracteres especiales

--- models/AdldapCreateForm.php
+++ models/AdldapCreateForm.php
@@ -36,7 +36,13 @@ class AdldapCreateForm extends Model
             [['department','title'],'string'],
             [['mobile','uac'],'number'],
             [['mail','personalmail'], 'email'],
-
+            [['mail','personalmail'], 'email'],
+            [['samaccountname'], 'match',
+                'pattern' => '/^[a-z0-9.]+$/u',
+                'message'=>'{attribute} no debe contener caracteres especiales, ni mayùscula'],
+            [['mail','personalmail'], 'match',
+                'pattern' => '/^[a-z0-9@.]+$/u',
+                'message'=>'{attribute} no debe contener caracteres especiales, ni mayùscula'],
         ];
     }
 
